3a05bb79292768d9b43e11ba04de570c31ecab83
JuhunC <31757586+JuhunC@users.noreply.github.com>
2018-07-25 21:56:44 +0900

    Update CoinChange.java
    Added Combination of Coins for Minimum Number of Coin

--- Dynamic Programming/CoinChange.java
+++ Dynamic Programming/CoinChange.java
@@ -13,7 +13,14 @@ public class CoinChange {
         int[] coins = {1, 2, 5};
 
         System.out.println("Number of combinations of getting change for " + amount + " is: " + change(coins, amount));
-        System.out.println("Min Number of Coins Used for " + amount + " is " + minChange(coins,amount));        
+        System.out.println("Combination is : ");
+        int[] result = minChange(coins,amount);
+        int sum =0;
+        for(int i =0;i<coins.length;i++) {
+        	sum += result[i];
+        	System.out.println(coins[i] + "coin : x" + result[i]);
+        }
+        System.out.println("Min Number of Coins Used for " + amount + " is " + sum);
     }
     /**
      * This method finds minimum amount of Coins
@@ -21,26 +28,43 @@ public class CoinChange {
      * @param amount The amount for which we need to find the change
      * @return Minimum amount of Coins
      */
-    public static int minChange(int[] coins, int amount) {
+    public static int[] minChange(int[] coins, int amount) {
     	int coin[] = new int[amount+1];
-    	for(int c : coins) {
-    		if(c <= amount)
-    		coin[c] = 1;
+    	int comb[][] = new int[amount+1][];
+    	
+    	// set Combination Array
+    	for(int i =0;i<amount+1;i++) {
+    		comb[i] = new int[coins.length];
+    	}
+    	
+    	// Init Combinations
+    	for(int i =0;i<coins.length;i++) {
+    		if(coins[i] <= amount) {
+    			coin[coins[i]] = 1;
+    			comb[coins[i]][i] = 1;
+    		}
     	}
     	for(int i =1;i<=amount;i++) {
-    		if(coin[i] != 1)
+    		if(coin[i] != 1) {
     			coin[i] = i;
+    			comb[i][0] = i;
+    		}
     	}
+    	
     	for(int i =1;i<=amount;i++) {
-    		for(int c : coins) {
-    			if(i-c >= 1) {
-    				if(coin[i-c]+1 < coin[i]) {
-    					coin[i] = coin[i-c] +1;
+    		for(int m=0;m<coins.length;m++) {
+    			if(i-coins[m] >= 1) {
+    				if(coin[i-coins[m]]+1 < coin[i]) {
+    					coin[i] = coin[i-coins[m]] +1;
+    					for(int k=0;k<coins.length;k++) {
+    						comb[i][k] = comb[i-coins[m]][k];
+    					}
+    					comb[i][m]++;
     				}
     			}
     		}
     	}
-    	return coin[amount];
+    	return comb[amount];
     }
     /**
      * This method finds the number of combinations of getting change for a given amount and change coins
